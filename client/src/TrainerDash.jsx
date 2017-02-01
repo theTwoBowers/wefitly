448b8ff5c4c3558c06d2c12eaad399aa5706a5bf
Scott Chao <svinets@gmail.com>
2017-01-31 16:16:56 -0800

    Create new table for confirmed training requests

--- client/src/TrainerDash.jsx
+++ client/src/TrainerDash.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import BookingTable from './booking.jsx'
+import BookingTable from './booking.jsx';
 import _ from 'lodash';
-import css from './home.css'
+import css from './home.css';
 
-import TrainerProfileEditor from './trainerProfile.jsx'
+import TrainerProfileEditor from './trainerProfile.jsx';
 
 class TrainerDash extends React.Component {
   constructor(props) {
@@ -11,17 +11,23 @@ class TrainerDash extends React.Component {
   }
 
   render() {
-
     return (
       <div className="dash-body">
-        <div className="dash-container w-container">
-          <h1>Pending Bookings</h1>
+
+        <div className="w-form">
+          <form className="update-profile-wrapper w-clearfix" id="email-form">
+            <input className="signupbutton w-button" type="submit" value="Update Your Profile" />
+          </form>
+        </div>
+        
+        <div className="dash-container w-col w-col-6" id="confirmed">
+          <h1 id="pendingTitle">Confirmed Bookings</h1>
           <BookingTable booking={this.props.bookings} rejectBooking={this.props.rejectBooking}/>
-          <div className="w-form">
-            <form className="update-profile-wrapper w-clearfix" id="email-form">
-              <input className="signupbutton w-button" type="submit" value="Update Your Profile" />
-            </form>
-          </div>
+        </div>
+
+        <div className="dash-container w-col-6" id="pending">
+          <h1 id="pendingTitle">Pending Bookings</h1>
+          <BookingTable booking={this.props.bookings} rejectBooking={this.props.rejectBooking}/>
         </div>
       </div>
     );
--- client/src/bookingRow.jsx
+++ client/src/bookingRow.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import css from './home.css'
+import css from './home.css';
 
 class BookingRow extends React.Component {
   constructor(props) {
@@ -7,7 +7,7 @@ class BookingRow extends React.Component {
   }
 
   renderActions() {
-    return(
+    return (
        <td>
         <button onClick={this.props.rejectBooking.bind(this, this.props.service)}>Reject</button>
       </td>
@@ -16,23 +16,27 @@ class BookingRow extends React.Component {
 
   render() {
     console.log(this.props);
-    return(
+    return (
 
-      <li className="testimonial-row">
+      <li className="testimonial-row" id="singleBooking">
         <div className="w-row">
-          <div className="booking-column w-col w-col-4">
+          <div className="booking-column w-col w-col-3">
             <div className="booking-row-title">
               <h4>Client</h4>
               <div>{this.props.userFirstname + ' ' + this.props.userLastname}</div>
             </div>
           </div>
-          <div className="w-col w-col-4">
+          <div className="w-col w-col-3">
             <h4>Service</h4>
             <div>{this.props.service}</div>
           </div>
-          <div className="w-col w-col-4">
+          <div className="w-col w-col-3">
             <h4>Duration</h4>
             <div>{this.props.duration}</div>
+          </div>
+          <div className="w-col w-col-3">
+            <h4>Date / Time</h4>
+            <div>Feb. 12 - 10:30am</div>
           </div>
         </div>
       </li>
