0c69738b232e30800307c28c58b7ca9ed1be7144
Scott Chao <svinets@gmail.com>
2017-02-03 11:41:55 -0800

    Create message section for bookings

--- client/src/confirmed.jsx
+++ client/src/confirmed.jsx
@@ -4,6 +4,10 @@ import css from './home.css';
 class Confirmed extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: ['Trainer: hello', 'Warrior: hello...', 'Trainer: ready to work hard?', 'Warrior: no.'],
+      visibility: {display: 'none'}
+    };
   }
 
   componentWillMount() {
@@ -28,6 +32,32 @@ class Confirmed extends React.Component {
     return out;
   } 
 
+  toggleMessages() {
+    console.log(this.state.visibility.display);
+    if (this.state.visibility.display === 'none') {
+      this.setState({
+        visibility: {display: 'unset'}
+      });
+    } else {
+      this.setState({
+        visibility: {display: 'none'}
+      });
+    }
+  }
+
+  writeMessage(e) {
+    e.preventDefault();
+    $.put('/api/chat', {
+      // isBooked: true,
+      // trainerName: this.props.firstName + ' ' + this.props.lastName,
+      // trainerEmail: this.props.email,
+      // service: this.refs.service.value,
+      // duration: this.refs.duration.value
+    }).done((results) => {
+      this.props.submitRequest();
+    });
+  }
+
   render() {
     console.log(this.props.date);
     return (
@@ -51,6 +81,17 @@ class Confirmed extends React.Component {
             <div className="w-col w-col-3">
               <h4>Date / Time</h4>
               <div>{this.correctDate(this.props.date)}</div>
+            </div>
+          </div>
+          <div className="w-row">
+            <span onClick={this.toggleMessages.bind(this)}>Messages</span>
+          </div>
+          <div className="w-row" style={this.state.visibility}>
+            <div className="w-col w-col-12" id="bookingMessages">
+              {this.state.messages.map((message, i) =>
+                <p key={i}>{message}</p>
+              )}
+              <input id="bookingInput"></input>
             </div>
           </div>
         </li>
