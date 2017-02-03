c23774ab388312039748809aae6b1833b3253107
Scott Chao <svinets@gmail.com>
2017-02-02 17:01:18 -0800

    Remove conflicts in TrainerDash

--- client/src/TrainerDash.jsx
+++ client/src/TrainerDash.jsx
@@ -108,14 +108,12 @@ class TrainerDash extends React.Component {
     console.log(this.props);
     return (
       <div className="dash-body">
-<<<<<<< 376ead4528ef63d345d3b567ff623782cc5328dd
         <button onClick={this.logout.bind(this)}>Logout</button>
-=======
+
         <div id="profileImage">
           <img className="profile-image" src={this.state.profileImage} />
         </div>
 
->>>>>>> Restrict email to account/Add img to trainer dash
         <div className="w-form">
           <form className="update-profile-wrapper w-clearfix" id="email-form">
             <input className="signupbutton w-button" type="submit" value="Update Your Profile" onClick={this.props.editProfile} />
