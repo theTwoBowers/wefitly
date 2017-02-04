538a652ca50257f840e12de8138e2c112ffb4086
Scott Chao <svinets@gmail.com>
2017-02-04 14:59:22 -0800

    Display times along with date in bookings

--- client/src/confirmed.jsx
+++ client/src/confirmed.jsx
@@ -29,10 +29,11 @@ class Confirmed extends React.Component {
   }
 
   correctDate(date) {
-    var d = date.substr(4, 1);
-    var c = date.substr(0, 4);
-    var out = date.substr(5, 5).concat(-c);
-    return out;
+    var newDate = date.split(' ');
+    var d = newDate[0].substr(4, 1);
+    var c = newDate[0].substr(0, 4);
+    var out = newDate[0].substr(5, 5).concat(-c);
+    return out + ' at ' + newDate[1];
   } 
 
   toggleMessages() {
